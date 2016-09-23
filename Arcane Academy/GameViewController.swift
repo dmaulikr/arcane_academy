7526988501093bef0e6df492fb1d92c2f03a5308
Patrick Gleeson <patrick.c.gleeson@googlemail.com>
2016-09-23 14:10:49 +0100

    Pilfered a smidgen from Swiftris to get started

--- Arcane Academy/GameViewController.swift
+++ Arcane Academy/GameViewController.swift
@@ -10,41 +10,21 @@ import UIKit
 import SpriteKit
 
 class GameViewController: UIViewController {
+    var scene: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
-    }
+        // Configure the view.
+        let skView = view as! SKView
+        skView.multipleTouchEnabled = false
 
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
+        // Create and configure the scene.
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
 
-    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return .AllButUpsideDown
-        } else {
-            return .All
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+        // Present the scene.
+        skView.presentScene(scene)
     }
 
     override func prefersStatusBarHidden() -> Bool {
